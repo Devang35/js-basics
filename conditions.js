cdadf4143203209ffe671ca9fa2e292dae246164
Devang <200040041@iitb.ac.in>
2022-11-23 06:55:43 +0000

    Create 3 files

---
+++ conditions.js
@@ -0,0 +1,4 @@
+const a = 5;
+if (a==1) console.log("a is true");
+else console.log("a is false");
+
---
+++ errorhandling.js
@@ -0,0 +1,9 @@
+try{
+    const a = 0
+    const b =17/0
+    console.log(b)
+} catch(weirdErr){
+    console.log(weirdErr)
+}
+
+//read more
